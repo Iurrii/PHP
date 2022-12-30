dbac8b6de81c9f6c8da7650bcd3a4faf25b88927
Iurrii <its_mail@programmer.net>
2022-12-30 21:38:33 +0200

    first commit

---
+++ project_01/components/footer/footer.php
@@ -0,0 +1,11 @@
+
+<style>
+    <?php
+        include_once __DIR__ . '/footer.css'
+    ?> 
+</style>
+
+
+<footer>
+    <p>Text in the footer 2</p>
+</footer>
---
+++ project_02/script.php
@@ -0,0 +1,18 @@
+<?php
+###task1
+function get_sum_without_return($a, $b, $c, $d, $e) {
+    print ($a + $b + $c + $d + $e);
+}
+
+get_sum_without_return(5, 52, 8, 9, 65);
+
+#need_a_separator_between_the_results_of_functions
+###task2
+function get_sum_with_return($f, $g, $h, $i, $j) {
+    $tmp = $f + $g + $h + $i + $j;
+    return $tmp;
+}
+print(get_sum_with_return(2, 5, 1, 8, 10))
+
+?>
+
